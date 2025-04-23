a2497a93e35cd95a8fe3d214638b56a704692c25
JongoSimon <simonjongo@gmail.com>
2025-04-23 09:51:17 -0500

    update

---
+++ JS/app.js
@@ -0,0 +1,19 @@
+for (let i = 0; i < 5; i++) {
+    console.log(i); // 0, 1, 2, 3, 4
+  }
+
+  for(let month = 1; month <=12; month++) {
+    console.log('month: ${month}')
+    for(let day = 1; day <=30; day++){
+        console.log('day: ${day}')
+    }
+  }
+
+  //? while loops
+
+  let i = 0;
+while (i < 5) {
+  console.log(i); // 0, 1, 2, 3, 4
+  i++;
+}
+
